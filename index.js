31a3febd52e75a599ae631e66d23a32166e517e4
Radha1403 <msradha93@gmail.com>
2020-03-17 12:21:02 +0530

    changed json

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res, next) => {
-  res.send({ hi: "hello" });
+  res.send({ bye: "boddy" });
 });
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
